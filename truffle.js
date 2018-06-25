66ef7253bdb7173b89734214c5639ddae96ffdd0
Ashley <apilipis@gmail.com>
2018-06-25 10:25:57 -0700

    Create truffle.js

---
+++ truffle.js
@@ -0,0 +1,16 @@
+module.exports = {
+    networks: {
+	development: {
+	    host: "localhost",
+	    port: 8545,
+	    network_id: "*" // Match any network id
+	},
+
+	ropsten: {
+	    host: "localhost",
+	    port: 8545,
+	    network_id: 3,
+	    gas: 4612388,
+	}
+    }
+};
